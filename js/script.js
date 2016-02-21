008995217b601ac9118bb9c700400c720f6e0dd8
Boaz Blake <boazblake@gmail.com>
2016-02-21 08:52:31 -0600

    need to switch from rgba to hex and sync progress bar to 0 seconds

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
-var clockStamp = document.querySelector('.container')
-console.log(clockStamp)
+var clockStamp = document.querySelector('.clockContainer')
+// console.log(clockStamp)
 
 var clock = clockStamp.querySelector('.clock')
-console.log(clock)
+// console.log(clock)
 
 var clockFace = document.createElement('div')
 
 clockFace.className += ' clockFace'
 
+
+
+
+    //////////////////////////////////////////////
+// clock
+// ///////////////////////////////////////////////
+
 var flashTextTop = function () {
 	var clockFace = document.querySelector('.clock')
 	var timeNow = new Date()
 	var hrs = timeNow.getHours()
+		hrs = ("0" + hrs).slice(-2);
+
 	var mins = timeNow.getMinutes()
+		mins = ("0" + mins).slice(-2);
+
 	var sec = timeNow.getSeconds()
+		sec = ("0" + sec).slice(-2);
     
     clockFace.innerHTML = hrs + ":" + mins + ":" + sec
     // console.log([time])
@@ -21,5 +33,63 @@ var flashTextTop = function () {
 }
 
      clock.appendChild(clockFace)
-
+    //////////////////////////////////////////////
+// blink function
+// ///////////////////////////////////////////////
  var intervalID = window.setInterval(flashTextTop, 1000);
+
+    //////////////////////////////////////////////
+// hover function
+// ///////////////////////////////////////////////
+
+function initElement()  
+    {      	
+    	var clockContainer = document.querySelector('.clockContainer')
+        clockContainer.onmouseover = showMouseOver
+        // clockclockContainer.onmouseout = showMouseOut;
+		// var backGrndCont = window.getComputedStyle(clockContainer, null).getPropertyValue("background-image");
+		// console.log([backGrndCont])
+    }
+
+    function showMouseOver()  
+    { 
+    	var clockContainer = document.querySelector('.clockContainer')
+    	// console.log([clockContainer])
+		// var backGrndCont = window.getComputedStyle(clockContainer, null).getPropertyValue("background-image");
+		var backGrndCont = window.getComputedStyle(clockContainer, null).getPropertyValue('background');
+    	console.log(backGrndCont)
+        return backGrndCont
+    }
+    
+    // function showMouseOut()
+    // {
+    //     var notice = document.querySelector("notice");
+    //     notice.innerHTML = 'mouse out detected';
+    // }
+
+
+
+
+
+
+
+
+
+    //////////////////////////////////////////////
+    				// progress Bar
+    //////////////////////////////////////////////
+
+    function move() {
+    var pBar = document.querySelector(".progressBar"); 
+    // console.log(pBar)
+    var width = 0;
+    var id = setInterval(frame, 600);
+    function frame() {
+        if (width === 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            pBar.style.width = width + '%'; 
+        }
+    }
+}

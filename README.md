2005452355553b8ec1648d44b16f584d3752f852
boazblake <boazblake@gmail.com>
2016-04-17 11:33:54 -0500

    heartbeet needed still

---
+++ README.md
@@ -0,0 +1,5 @@
+This app explores the concept of set intervals and how JS can be used to manipulate the DOM dynamicly.
+
+when the mouse hovers over the time, the clock chnages into the RGB equivilant of the background, which in turn is defined by the actual time.
+
+This is done by correspoending the 3 parts of the RGB color scheme and the 3 part HH:MM:SS.
--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
+
+// Creating the clock
 var clock = document.querySelector('.clock')
-
+// creating the clock face
 var clockFace = document.createElement('div')
-
-clockFace.className += ' clockFace'
+// giving the clockface the class of 'clockFace' to enable me to style it in the CSS
+clockFace.className += 'clockFace'
 
 ////////////
 // Clock //
 ///////////
 
+// Adding the clockFace to the clock
 clock.appendChild(clockFace)
 
 ///////////////////////
 // helper function //
 ///////////////////////
 
+// This function captures the sync between the time and Hex color change in an object.
+// Taking advatnge of the 3 part RGB and 3 part H:M:S.
 function timeToRGB(hrs,mins,sec) {
 	var objectRGB = {}
 	objectRGB.red = Math.floor((255 * hrs) / 24)
@@ -25,17 +30,23 @@ function timeToRGB(hrs,mins,sec) {
 ///////////////////////
 // functions to call //
 ///////////////////////
+///
+///Placing the Hex Color numerals on the DOM. The function takes the input of the RGB object and splits it into R:G:B
 function setHexColorTime(rgbObject) {
     var hexContain = document.querySelector('.hexContainer')
     var hexColor = rgbObject.red.toString(16) + ":" + rgbObject.green.toString(16) + ":" + rgbObject.blue.toString(16)
     hexContain.textContent = hexColor
 }
 
-function backgroundChange(rgbO){
+// changing the background : this function also takes in the RBG Object and uses it to change the style of the backgound in RGB.
+function backgroundChange(rgbObject){
     var body = document.querySelector('body')
-    body.style.background = 'rgb(' + rgbO.red + ',' + rgbO.green + ',' + rgbO.blue +')'
+    body.style.background = 'rgb(' + rgbObject.red + ',' + rgbObject.green + ',' + rgbObject.blue +')'
 }
 
+
+// This function defines how the clock is to look and adds it to the DOM. I chose to have a 24 hour clock and so appended an initial 0 and sliced off the 
+// 
 function setClockTime(hrs,mins,sec) {
 	    hrs = ("0" + hrs).slice(-2);
 	    mins = ("0" + mins).slice(-2);
@@ -43,12 +54,15 @@ function setClockTime(hrs,mins,sec) {
 	    clockFace.innerHTML = hrs + ":" + mins + ":" + sec
 }
 
+// The progrgress bar is incremented every sec by 1/60th
 function setProgressBarWidth(sec) {
  	var pBar = document.querySelector(".progressBar");
     var percentSec = sec / 60 * 100
     pBar.style.width = percentSec + "%";
 }
 
+
+// I still need to edit the styling and the set-interval to look more like a heart beat!
 function heartBeat(sec) {
  	var heart = document.querySelector(".heart");
  	console.log(heart.style.backgroundImage)
@@ -56,7 +70,7 @@ function heartBeat(sec) {
 }
 
 
-//////////////////////////////////////////
+///// This function defines the current time and invokes the rest of the functions
 function getTime() {
     var timeNow = new Date()
     var hours = timeNow.getHours()
@@ -77,5 +91,6 @@ function getTime() {
 // Timer function //
 ////////////////////
 
+// this function invokes a set interval for every 1s to invoke the get time function - which in turn invokes the hex color , time progress bar and 'heartbeat'
 
 var intervalID = window.setInterval(getTime, 1000);
